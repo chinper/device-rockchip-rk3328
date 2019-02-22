6bae08bf348e8727e2ac3f51822e13b889a808d9
Chinper Lin <chinper@gmail.com>
2019-02-22 22:46:54 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@ GAPPS_PRODUCT_PACKAGES += Chrome
 GAPPS_EXCLUDED_PACKAGES += BugReportSender
 GAPPS_EXCLUDED_PACKAGES += GlobalKeyInterceptor
 GAPPS_EXCLUDED_PACKAGES += RemoteControlService
+GAPPS_EXCLUDED_PACKAGES += AtvRemoteService
 #
 PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=wlan0 \
