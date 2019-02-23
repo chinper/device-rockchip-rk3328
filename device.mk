1fe88447e1be1dc3b9499a061a20a629c634d5bd
Chinper Lin <chinper@gmail.com>
2019-02-24 05:04:25 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+GAPPS_EXCLUDED_PACKAGES += BugReportSender
+#
 PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=wlan0 \
     ro.opengles.version=196609
